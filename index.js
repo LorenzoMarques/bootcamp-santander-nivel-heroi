e61f20e0c97e042d584d9c6d57b6297c67c2a176
Lorenzo Marques <lorenzo.marques246@gmail.com>
2024-04-25 18:09:21 -0300

    feat/loop

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-let nome = "Artur";
-let xp = 4200;
+const herois = [
+  { nome: "Artur", xp: 4500 },
+  { nome: "Liana", xp: 900 },
+  { nome: "Mirabel", xp: 3000 },
+  { nome: "Jonas", xp: 7500 },
+  { nome: "Eva", xp: 9500 },
+  { nome: "Lorenzo", xp: 10002 },
+];
 
-let nivel;
-
-switch (true) {
-  case xp < 1000:
-    nivel = "Ferro";
-    break;
-  case xp <= 2000:
-    nivel = "Bronze";
-    break;
-  case xp <= 5000:
-    nivel = "Prata";
-    break;
-  case xp <= 7000:
-    nivel = "Ouro";
-    break;
-  case xp <= 8000:
-    nivel = "Platina";
-    break;
-  case xp <= 9000:
-    nivel = "Ascendente";
-    break;
-  case xp <= 10000:
-    nivel = "Imortal";
-    break;
-  default:
-    nivel = "Radiante";
-    break;
+function determinarNivel(xp) {
+  switch (true) {
+    case xp < 1000:
+      return "Ferro";
+    case xp <= 2000:
+      return "Bronze";
+    case xp <= 5000:
+      return "Prata";
+    case xp <= 7000:
+      return "Ouro";
+    case xp <= 8000:
+      return "Platina";
+    case xp <= 9000:
+      return "Ascendente";
+    case xp <= 10000:
+      return "Imortal";
+    default:
+      return "Radiante";
+  }
 }
 
-console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+for (let i = 0; i < herois.length; i++) {
+  console.log(
+    `O Herói de nome ${herois[i].nome} está no nível de ${determinarNivel(
+      herois[i].xp
+    )}`
+  );
+}
